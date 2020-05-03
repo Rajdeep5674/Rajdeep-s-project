2d1ade970dae6625a2cd0f541c9c19ea1efb0c77
Rajdeep5674 <64572542+Rajdeep5674@users.noreply.github.com>
2020-05-03 20:15:54 +0530

    03/05/2020

--- src/app/create-user-page/create-user-page.component.ts
+++ src/app/create-user-page/create-user-page.component.ts
@@ -1,13 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
+import {Location} from '@angular/common';
 @Component({
   selector: 'app-create-user-page',
   templateUrl: './create-user-page.component.html',
   styleUrls: ['./create-user-page.component.css']
 })
 export class CreateUserPageComponent implements OnInit {
+private full_name;
+private full_address;
+private dob;
+private phone_number;
+private username;
+private password1;
+private password2;
 
-  constructor() { }
+@Output()
+public childEvent=new EventEmitter();
+
+  submit(full_name,full_address,dob,phone_number,username,password1,password2){
+  this.full_name=full_name;
+  this.full_address=full_address;
+  this.dob=dob;
+  this.phone_number=phone_number;
+  this.username=username;
+  this.password1=password1;
+  this.password2=password2;
+  }
+  fireEvent()
+  {
+    this.childEvent.emit(true);
+  }
+  close()
+  {
+    this._location.back();
+  }
+  constructor(private _location:Location) { 
+    
+  }
 
   ngOnInit() {
   }
