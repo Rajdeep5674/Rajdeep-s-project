c2ada9bd154df270fa10a5aaef093852b689d910
Rajdeep5674 <64572542+Rajdeep5674@users.noreply.github.com>
2020-05-03 20:20:55 +0530

    03/05/2020

--- src/app/approuting.module.ts
+++ src/app/approuting.module.ts
@@ -6,6 +6,7 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { SuccessfulLoginComponent } from './successful-login/successful-login.component';
 import { MessagePageComponent } from './message-page/message-page.component';
 import { CreateUserPageComponent } from './create-user-page/create-user-page.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
@@ -14,10 +15,11 @@ import { CreateUserPageComponent } from './create-user-page/create-user-page.com
       { path: 'home', component: HomepageComponent },
       { path: 'SuccessfulLogin' , component: SuccessfulLoginComponent},
       { path: 'MessagePage' , component: MessagePageComponent},
-      { path: 'CreateUserPage' , component: CreateUserPageComponent}
+      { path: 'CreateUserPage' , component: CreateUserPageComponent},
+      { path: 'root' , component: AppComponent}
     ])
   ],
-  declarations: [HomepageComponent,SuccessfulLoginComponent,MessagePageComponent,CreateUserPageComponent],
+  declarations: [HomepageComponent,SuccessfulLoginComponent,MessagePageComponent,CreateUserPageComponent,AppComponent],
   exports:[
     RouterModule,
   ],
